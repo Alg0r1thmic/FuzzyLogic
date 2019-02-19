ccb9324528252a5af11a8ef5c4bb593c5d3528ab
Alg0r1thmic <djbrexman@gmail.com>
2019-02-19 16:58:12 -0500

    cristopher se la come

--- FuzzyLogic/mainwindow.cpp
+++ FuzzyLogic/mainwindow.cpp
@@ -7,6 +7,21 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //set configurations
+    ui->customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectAxes |
+                                    QCP::iSelectLegend | QCP::iSelectPlottables);
+    ui->customPlot->plotLayout()->insertRow(0);
+    QCPTextElement *title = new QCPTextElement(ui->customPlot, "Fuzzy Logic Interaction Example", QFont("sans", 17, QFont::Bold));
+    ui->customPlot->plotLayout()->addElement(0, 0, title);
+
+    //set font
+    ui->customPlot->legend->setVisible(true);
+    QFont legendFont = font();
+    legendFont.setPointSize(10);
+    ui->customPlot->legend->setFont(legendFont);
+    ui->customPlot->legend->setSelectedFont(legendFont);
+    ui->customPlot->legend->setSelectableParts(QCPLegend::spItems); // legend box shall not be selectable, only legend items
+
 }
 
 MainWindow::~MainWindow()
@@ -26,10 +41,15 @@ void MainWindow::setupDemo(int demoIndex)
 void MainWindow::setupQuadraticDemo(QCustomPlot *customPlot)
 {
     QVector<double> x(1001),y(1001);
+    for (int i=0; i<101; ++i)
+    {
+      x[i] = i/50.0 - 1; // x goes from -1 to 1
+      y[i] = x[i]*x[i];  // let's plot a quadratic function
+    }
     customPlot->addGraph();
     customPlot->graph(0)->setData(x,y);
-    customPlot->xAxis->setLabel("x");
-    customPlot->yAxis->setLabel("y");
+    customPlot->xAxis->setLabel("X");
+    customPlot->yAxis->setLabel("Y");
     // set axes ranges, so we see all data:
     customPlot->xAxis->setRange(this->xP, this->xN);
     customPlot->yAxis->setRange(this->yP, this->yN);
@@ -43,4 +63,25 @@ void MainWindow::on_pushButton_clicked()
     this->xN=ui->inputXN->text().toInt();
     this->yN=ui->inputYN->text().toInt();
     setupDemo(0);
+}
+//add function gamma
+void MainWindow::on_pushButton_5_clicked()
+{
+    setupDemo(0);
+}
+//add function L
+void MainWindow::on_pushButton_7_clicked()
+{
+
+}
+//add function Lambda or triangle
+void MainWindow::on_pushButton_8_clicked()
+{
+
+}
+
+//add function PI o trapezoidal
+void MainWindow::on_pushButton_9_clicked()
+{
+
 }
--- build-FuzzyLogic-Desktop_Qt_5_11_2_GCC_64bit-Debug/ui_mainwindow.h
+++ build-FuzzyLogic-Desktop_Qt_5_11_2_GCC_64bit-Debug/ui_mainwindow.h
@@ -17,6 +17,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
@@ -29,8 +30,32 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QCustomPlot *customPlot;
-    QPushButton *pushButton;
-    QWidget *widget;
+    QLabel *label_7;
+    QWidget *layoutWidget;
+    QGridLayout *gridLayout_3;
+    QGridLayout *gridLayout_7;
+    QSpacerItem *verticalSpacer_7;
+    QSpacerItem *verticalSpacer_8;
+    QLabel *label_11;
+    QPushButton *pushButton_5;
+    QGridLayout *gridLayout_9;
+    QSpacerItem *verticalSpacer_11;
+    QSpacerItem *verticalSpacer_12;
+    QLabel *label_13;
+    QPushButton *pushButton_7;
+    QGridLayout *gridLayout_10;
+    QSpacerItem *verticalSpacer_13;
+    QSpacerItem *verticalSpacer_14;
+    QLabel *label_14;
+    QPushButton *pushButton_8;
+    QGridLayout *gridLayout_11;
+    QSpacerItem *verticalSpacer_15;
+    QSpacerItem *verticalSpacer_16;
+    QLabel *label_15;
+    QPushButton *pushButton_9;
+    QWidget *layoutWidget1;
+    QGridLayout *gridLayout_5;
+    QGridLayout *gridLayout_2;
     QGridLayout *gridLayout;
     QLabel *label;
     QLabel *label_2;
@@ -41,6 +66,16 @@ public:
     QLineEdit *inputYN;
     QLineEdit *inputX;
     QLineEdit *inputY;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *pushButton;
+    QSpacerItem *horizontalSpacer_2;
+    QSpacerItem *horizontalSpacer_5;
+    QSpacerItem *verticalSpacer_2;
+    QGridLayout *gridLayout_4;
+    QSpacerItem *horizontalSpacer_3;
+    QLabel *label_6;
+    QSpacerItem *horizontalSpacer_4;
+    QSpacerItem *verticalSpacer;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -59,62 +94,222 @@ public:
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         customPlot = new QCustomPlot(centralWidget);
         customPlot->setObjectName(QStringLiteral("customPlot"));
-        customPlot->setGeometry(QRect(40, 110, 741, 441));
-        pushButton = new QPushButton(centralWidget);
-        pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(220, 90, 80, 20));
-        widget = new QWidget(centralWidget);
-        widget->setObjectName(QStringLiteral("widget"));
-        widget->setGeometry(QRect(40, 21, 446, 60));
-        gridLayout = new QGridLayout(widget);
+        customPlot->setGeometry(QRect(40, 140, 811, 411));
+        label_7 = new QLabel(centralWidget);
+        label_7->setObjectName(QStringLiteral("label_7"));
+        label_7->setGeometry(QRect(1040, 430, 49, 14));
+        layoutWidget = new QWidget(centralWidget);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        layoutWidget->setGeometry(QRect(920, 140, 431, 251));
+        gridLayout_3 = new QGridLayout(layoutWidget);
+        gridLayout_3->setSpacing(6);
+        gridLayout_3->setContentsMargins(11, 11, 11, 11);
+        gridLayout_3->setObjectName(QStringLiteral("gridLayout_3"));
+        gridLayout_3->setContentsMargins(0, 0, 0, 0);
+        gridLayout_7 = new QGridLayout();
+        gridLayout_7->setSpacing(6);
+        gridLayout_7->setObjectName(QStringLiteral("gridLayout_7"));
+        verticalSpacer_7 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_7->addItem(verticalSpacer_7, 0, 2, 1, 1);
+
+        verticalSpacer_8 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_7->addItem(verticalSpacer_8, 2, 2, 1, 1);
+
+        label_11 = new QLabel(layoutWidget);
+        label_11->setObjectName(QStringLiteral("label_11"));
+
+        gridLayout_7->addWidget(label_11, 0, 0, 3, 2);
+
+        pushButton_5 = new QPushButton(layoutWidget);
+        pushButton_5->setObjectName(QStringLiteral("pushButton_5"));
+
+        gridLayout_7->addWidget(pushButton_5, 1, 2, 1, 1);
+
+
+        gridLayout_3->addLayout(gridLayout_7, 0, 0, 1, 1);
+
+        gridLayout_9 = new QGridLayout();
+        gridLayout_9->setSpacing(6);
+        gridLayout_9->setObjectName(QStringLiteral("gridLayout_9"));
+        verticalSpacer_11 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_9->addItem(verticalSpacer_11, 0, 2, 1, 1);
+
+        verticalSpacer_12 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_9->addItem(verticalSpacer_12, 2, 2, 1, 1);
+
+        label_13 = new QLabel(layoutWidget);
+        label_13->setObjectName(QStringLiteral("label_13"));
+
+        gridLayout_9->addWidget(label_13, 0, 0, 3, 2);
+
+        pushButton_7 = new QPushButton(layoutWidget);
+        pushButton_7->setObjectName(QStringLiteral("pushButton_7"));
+
+        gridLayout_9->addWidget(pushButton_7, 1, 2, 1, 1);
+
+
+        gridLayout_3->addLayout(gridLayout_9, 0, 1, 1, 1);
+
+        gridLayout_10 = new QGridLayout();
+        gridLayout_10->setSpacing(6);
+        gridLayout_10->setObjectName(QStringLiteral("gridLayout_10"));
+        verticalSpacer_13 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_10->addItem(verticalSpacer_13, 0, 2, 1, 1);
+
+        verticalSpacer_14 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_10->addItem(verticalSpacer_14, 2, 2, 1, 1);
+
+        label_14 = new QLabel(layoutWidget);
+        label_14->setObjectName(QStringLiteral("label_14"));
+
+        gridLayout_10->addWidget(label_14, 0, 0, 3, 2);
+
+        pushButton_8 = new QPushButton(layoutWidget);
+        pushButton_8->setObjectName(QStringLiteral("pushButton_8"));
+
+        gridLayout_10->addWidget(pushButton_8, 1, 2, 1, 1);
+
+
+        gridLayout_3->addLayout(gridLayout_10, 1, 0, 1, 1);
+
+        gridLayout_11 = new QGridLayout();
+        gridLayout_11->setSpacing(6);
+        gridLayout_11->setObjectName(QStringLiteral("gridLayout_11"));
+        verticalSpacer_15 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_11->addItem(verticalSpacer_15, 0, 2, 1, 1);
+
+        verticalSpacer_16 = new QSpacerItem(20, 18, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_11->addItem(verticalSpacer_16, 2, 2, 1, 1);
+
+        label_15 = new QLabel(layoutWidget);
+        label_15->setObjectName(QStringLiteral("label_15"));
+
+        gridLayout_11->addWidget(label_15, 0, 0, 3, 2);
+
+        pushButton_9 = new QPushButton(layoutWidget);
+        pushButton_9->setObjectName(QStringLiteral("pushButton_9"));
+
+        gridLayout_11->addWidget(pushButton_9, 1, 2, 1, 1);
+
+
+        gridLayout_3->addLayout(gridLayout_11, 1, 1, 1, 1);
+
+        layoutWidget1 = new QWidget(centralWidget);
+        layoutWidget1->setObjectName(QStringLiteral("layoutWidget1"));
+        layoutWidget1->setGeometry(QRect(40, 0, 1311, 112));
+        gridLayout_5 = new QGridLayout(layoutWidget1);
+        gridLayout_5->setSpacing(6);
+        gridLayout_5->setContentsMargins(11, 11, 11, 11);
+        gridLayout_5->setObjectName(QStringLiteral("gridLayout_5"));
+        gridLayout_5->setContentsMargins(0, 0, 0, 0);
+        gridLayout_2 = new QGridLayout();
+        gridLayout_2->setSpacing(6);
+        gridLayout_2->setObjectName(QStringLiteral("gridLayout_2"));
+        gridLayout = new QGridLayout();
         gridLayout->setSpacing(6);
-        gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
-        gridLayout->setContentsMargins(0, 0, 0, 0);
-        label = new QLabel(widget);
+        label = new QLabel(layoutWidget1);
         label->setObjectName(QStringLiteral("label"));
 
         gridLayout->addWidget(label, 0, 0, 1, 1);
 
-        label_2 = new QLabel(widget);
+        label_2 = new QLabel(layoutWidget1);
         label_2->setObjectName(QStringLiteral("label_2"));
 
         gridLayout->addWidget(label_2, 1, 0, 1, 1);
 
-        label_3 = new QLabel(widget);
+        label_3 = new QLabel(layoutWidget1);
         label_3->setObjectName(QStringLiteral("label_3"));
 
         gridLayout->addWidget(label_3, 1, 1, 1, 1);
 
-        label_4 = new QLabel(widget);
+        label_4 = new QLabel(layoutWidget1);
         label_4->setObjectName(QStringLiteral("label_4"));
 
         gridLayout->addWidget(label_4, 1, 2, 1, 1);
 
-        label_5 = new QLabel(widget);
+        label_5 = new QLabel(layoutWidget1);
         label_5->setObjectName(QStringLiteral("label_5"));
 
         gridLayout->addWidget(label_5, 1, 3, 1, 1);
 
-        inputXN = new QLineEdit(widget);
+        inputXN = new QLineEdit(layoutWidget1);
         inputXN->setObjectName(QStringLiteral("inputXN"));
 
         gridLayout->addWidget(inputXN, 2, 0, 1, 1);
 
-        inputYN = new QLineEdit(widget);
+        inputYN = new QLineEdit(layoutWidget1);
         inputYN->setObjectName(QStringLiteral("inputYN"));
 
         gridLayout->addWidget(inputYN, 2, 1, 1, 1);
 
-        inputX = new QLineEdit(widget);
+        inputX = new QLineEdit(layoutWidget1);
         inputX->setObjectName(QStringLiteral("inputX"));
 
         gridLayout->addWidget(inputX, 2, 2, 1, 1);
 
-        inputY = new QLineEdit(widget);
+        inputY = new QLineEdit(layoutWidget1);
         inputY->setObjectName(QStringLiteral("inputY"));
 
         gridLayout->addWidget(inputY, 2, 3, 1, 1);
+
+
+        gridLayout_2->addLayout(gridLayout, 0, 0, 1, 3);
+
+        horizontalSpacer = new QSpacerItem(168, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout_2->addItem(horizontalSpacer, 1, 0, 1, 1);
+
+        pushButton = new QPushButton(layoutWidget1);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+
+        gridLayout_2->addWidget(pushButton, 1, 1, 1, 1);
+
+        horizontalSpacer_2 = new QSpacerItem(178, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout_2->addItem(horizontalSpacer_2, 1, 2, 1, 1);
+
+
+        gridLayout_5->addLayout(gridLayout_2, 0, 0, 3, 1);
+
+        horizontalSpacer_5 = new QSpacerItem(398, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout_5->addItem(horizontalSpacer_5, 0, 1, 2, 1);
+
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_5->addItem(verticalSpacer_2, 0, 2, 1, 1);
+
+        gridLayout_4 = new QGridLayout();
+        gridLayout_4->setSpacing(6);
+        gridLayout_4->setObjectName(QStringLiteral("gridLayout_4"));
+        horizontalSpacer_3 = new QSpacerItem(88, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout_4->addItem(horizontalSpacer_3, 0, 0, 1, 1);
+
+        label_6 = new QLabel(layoutWidget1);
+        label_6->setObjectName(QStringLiteral("label_6"));
+
+        gridLayout_4->addWidget(label_6, 0, 1, 1, 1);
+
+        horizontalSpacer_4 = new QSpacerItem(88, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        gridLayout_4->addItem(horizontalSpacer_4, 0, 2, 1, 1);
+
+
+        gridLayout_5->addLayout(gridLayout_4, 1, 2, 1, 1);
+
+        verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        gridLayout_5->addItem(verticalSpacer, 2, 2, 1, 1);
 
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
@@ -136,12 +331,22 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
-        pushButton->setText(QApplication::translate("MainWindow", "OK", nullptr));
+        label_7->setText(QApplication::translate("MainWindow", "Rules", nullptr));
+        label_11->setText(QApplication::translate("MainWindow", "Gamma", nullptr));
+        pushButton_5->setText(QApplication::translate("MainWindow", "Add", nullptr));
+        label_13->setText(QApplication::translate("MainWindow", "l", nullptr));
+        pushButton_7->setText(QApplication::translate("MainWindow", "Add", nullptr));
+        label_14->setText(QApplication::translate("MainWindow", "triangular", nullptr));
+        pushButton_8->setText(QApplication::translate("MainWindow", "Add", nullptr));
+        label_15->setText(QApplication::translate("MainWindow", "trapesoidal", nullptr));
+        pushButton_9->setText(QApplication::translate("MainWindow", "Add", nullptr));
         label->setText(QApplication::translate("MainWindow", "Set configurations", nullptr));
         label_2->setText(QApplication::translate("MainWindow", "-X", nullptr));
         label_3->setText(QApplication::translate("MainWindow", "-Y", nullptr));
         label_4->setText(QApplication::translate("MainWindow", "X", nullptr));
         label_5->setText(QApplication::translate("MainWindow", "Y", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "OK", nullptr));
+        label_6->setText(QApplication::translate("MainWindow", "Fuzzy Functions", nullptr));
     } // retranslateUi
 
 };
